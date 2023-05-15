a1ed21054f8c1a4f8eac588bc961ad25609b7171
Michal Zdanowicz <zdanowicz.michal24@gmail.com>
2023-05-15 13:01:17 -0400

    05/15

--- app/src/main/java/com/example/grzegorzbrzeczysczykiewicz/HomeFragment.kt
+++ app/src/main/java/com/example/grzegorzbrzeczysczykiewicz/HomeFragment.kt
@@ -11,12 +11,12 @@ import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.grzegorzbrzeczysczykiewicz.databinding.FragmentHomeBinding
-
-import com.google.firebase.database.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-
-
 
 class HomeFragment : Fragment() {
 
@@ -25,19 +25,89 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
     lateinit var email: String
     lateinit var password: String
+    private lateinit var auth: FirebaseAuth
+
     //private val viewModel: GuesserViewModel by viewModels()
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         dbRef = Firebase.database.reference
+        auth = FirebaseAuth.getInstance()
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         // Initialize Firebase Auth
+        val user = Firebase.auth.currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = it.displayName
+            val email = it.email
+            // Check if user's email is verified
+            val emailVerified = it.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            val uid = it.uid
+        }
+
         binding.button.setOnClickListener {
             val action = HomeFragmentDirections.actionHomeFragmentToGuesserFragment()
             binding.root.findNavController().navigate(action)
         }
-
+        binding.signup.setOnClickListener{
+            createAccount(binding.username.text.toString(), binding.password.text.toString())
+        }
         return binding.root
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+//        if (currentUser != null) {
+//            reload()
+//        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        context,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        context,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+        // [END sign_in_with_email]
     }
 
     override fun onDestroyView() {
